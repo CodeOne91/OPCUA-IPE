8d29a61b8501f91ca202328548eed72e4a2ed67f
CodeOne91 <code.one91@gmail.com>
2020-01-26 19:02:03 +0100

    -improve mapping

--- InterworkingManager.py
+++ InterworkingManager.py
@@ -1,155 +1,205 @@
 from IpeAe import IpeAe
 from opcua import ua
 import os.path
-import sys
 import opcua
 from openmtc_onem2m.model import CSEBase, AE, Container, ContentInstance
-
-
-sys.path.insert(0, "..")
-
-
-class InterworkingManager:
-    aeNodes = []
-    cseBaseNodes= []
-    containerNodes = []
+from CustomSession import CustomInternalServer
+from openmtc_onem2m.transport import OneM2MRequest
+import json
+from threading import Thread
+import time
+class InterworkingManager(Thread):
+    
     
     def __init__(self, xae):
         self.xae = xae
+        self.aeNodes = []
+        self.cseBaseNodes= []
+        self.containerNodes = []
+        self.nodeid_uri_dict = {}
+        self.nodeid_attr_dict = {}
+        self.opc_openmtc_attrname_dict = self.parse_json()
         
-
-
-    def mapResourceToNode(self,server):
-        self.nodeBuilder(self.xae.resourceDiscovered, server)
-
-            
-
-    def resourcenodeBuilder(self, myobj,resource):
+    def map_resource_to_node(self,server):
+        self.node_builder(self.xae.resourceDiscovered, server)
+        
+    def resource_node_builder(self, myobj,resource):
         listChildren = myobj.get_children()
-        for child in listChildren:
-           
+        for child in listChildren:           
             if child.get_browse_name().to_string() == "2:resourceName":
                 child.set_value(resource.resourceName)
-            if child.get_browse_name().to_string() == "2:resourceType":
+            elif child.get_browse_name().to_string() == "2:resourceType":
                 child.set_value(resource.resourceType)
-            if child.get_browse_name().to_string() == "2:resourceID":
+            elif child.get_browse_name().to_string() == "2:resourceID":
                 child.set_value(resource.resourceID)
-            if child.get_browse_name().to_string() == "2:parentID":
+            elif child.get_browse_name().to_string() == "2:parentID":
                 child.set_value(resource.parentID)
-            if child.get_browse_name().to_string() == "2:creationTime":
+            elif child.get_browse_name().to_string() == "2:creationTime":
                 child.set_value(resource.creationTime)
-            if child.get_browse_name().to_string() == "2:lastModifiedItem":
+            elif child.get_browse_name().to_string() == "2:lastModifiedTime":
                 child.set_value(resource.lastModifiedTime)
-        
           
-            
-            
-        
-    def cseNodeBuilder(self,server, resource):
+    def cse_node_builder(self,server, resource):
         idx = server.get_namespace_index("http://dieei.unict.it/oneM2M-OPCUA/")
         cseBaseObjectType = ua.NodeId.from_string('ns=%d;i=1003' % idx)
-        myobj = server.nodes.objects.add_object(idx, "cseBase", cseBaseObjectType)
-                
+        myobj = server.nodes.objects.add_object(idx, resource.resourceName, cseBaseObjectType)            
         listChildren = myobj.get_children()
         for child in listChildren:
-            if child.get_browse_name().to_string() == "2:cseID":
+            self.populate_dict_name(child)                    
+            if child.get_browse_name().to_string() == "2:CSE-ID":
                 child.set_value(resource.CSE_ID)
-            if child.get_browse_name().to_string() == "2:cseType":
+            elif child.get_browse_name().to_string() == "2:cseType":
                 child.set_value(resource.cseType-1)
-            
-                         
-        self.resourcenodeBuilder(myobj, resource)
+        self.resource_node_builder(myobj, resource)
+        if resource.resourceID is not None:
+            self.populate_dict(myobj,resource)
+        
         return myobj
     
-      #inserire meccanismo di creazione del resource tree, quindi sotto il cseBase (magari considerando il parentID)  
-    def aeNodeBuilder(self,myobj, resource):
-        #entrare dentro <ae>
+    def ae_node_builder(self,myobj, resource):
         idx = server.get_namespace_index("http://dieei.unict.it/oneM2M-OPCUA/")
         aeObjectType = ua.NodeId.from_string('ns=%d;i=1007' % idx)
         
         aeNode = myobj.add_object(idx,resource.resourceName, aeObjectType)
         listChildren = aeNode.get_children()
         for child in listChildren:
-            if child.get_browse_name().to_string() == "2:ae-ID":
+            self.populate_dict_name(child)                    
+            if child.get_browse_name().to_string() == "2:AE-ID":
                 child.set_value(resource.AE_ID)
-            if child.get_browse_name().to_string() == "2:app-ID":
-                child.set_value(resource.App_ID)
-                            
-        self.resourcenodeBuilder(aeNode, resource)
+            elif child.get_browse_name().to_string() == "2:App-ID":
+                child.set_value(resource.App_ID)                            
+        self.resource_node_builder(aeNode, resource)
+        self.populate_dict(aeNode, resource)
+
         return aeNode
-        
-    def containerNodeBuilder(self,aeNodesList, resource):
+    
+    #da migliorare   
+    
+    def container_node_builder(self,aeNodesList, containerNodesList, resource):
         idx = server.get_namespace_index("http://dieei.unict.it/oneM2M-OPCUA/")
         containerObjectType = ua.NodeId.from_string('ns=%d;i=1005' % idx)
+        listChildren = []
         for aeNode in aeNodesList:
             if resource.parentID == aeNode.get_child("2:resourceID").get_value():
-                containerNode = aeNode.add_object(idx,resource.resourceName, containerObjectType)
-                listChildren = containerNode.get_children()
+                containerNodeAdded = aeNode.add_object(idx,resource.resourceName, containerObjectType)
+                listChildren = containerNodeAdded.get_children()
+        for containerNode in containerNodesList:
+            print(containerNode)
+            if resource.parentID == containerNode.get_child("2:resourceID").get_value():
+                containerNodeAdded = containerNode.add_object(idx,resource.resourceName, containerObjectType)
+                print("CNT:" + str(containerNode.get_child("2:resourceID").get_value())+ "  ADD AS CHILD: "+ resource.resourceName)
+                listChildren = containerNodeAdded.get_children()
+        
+        for child in listChildren:
+            self.populate_dict_name(child)
+            if child.get_browse_name().to_string() == "2:creationTime":
+                child.set_value(resource.creationTime)
+            elif child.get_browse_name().to_string() == "2:currentNrOfInstances":
+                child.set_value(resource.currentNrOfInstances)
+        self.resource_node_builder(containerNodeAdded, resource)
+        self.populate_dict(containerNodeAdded, resource)
 
-                for child in listChildren:
-                    if child.get_browse_name().to_string() == "2:creationTime":
-                        child.set_value(resource.creationTime)
-                    if child.get_browse_name().to_string() == "2:currentNrOfInstances":
-                        child.set_value(resource.currentNrOfInstances)
-                self.resourcenodeBuilder(containerNode, resource)
-                return containerNode
+        return containerNodeAdded
                 
-    def contentInstanceNodeBuilder(self,containerNodesList, resource):
+    def content_instance_node_builder(self,containerNodesList, resource):
         idx = server.get_namespace_index("http://dieei.unict.it/oneM2M-OPCUA/")
         contentInstanceObjectType = ua.NodeId.from_string('ns=%d;i=1004' % idx)
         for containerNode in containerNodesList:
             if resource.parentID == containerNode.get_child("2:resourceID").get_value():
                 contentInstanceNode = containerNode.add_object(idx,resource.resourceName, contentInstanceObjectType)
+                print("CNT for Inst:" + str(containerNode.get_child("2:resourceID").get_value())+ "  ADD AS CHILD: "+ resource.resourceName)
+
                 listChildren = contentInstanceNode.get_children()
-
                 for child in listChildren:
+                    self.populate_dict_name(child)                    
                     if child.get_browse_name().to_string() == "2:content":
-
                         child.set_value(resource.content.decode("utf-8"))
-                    if child.get_browse_name().to_string() == "2:contentSize":
+                    elif child.get_browse_name().to_string() == "2:contentSize":
                         child.set_value(resource.contentSize)
-                self.resourcenodeBuilder(contentInstanceNode, resource)
+                self.resource_node_builder(contentInstanceNode, resource)
+                self.populate_dict(contentInstanceNode, resource)
+               
+    def populate_dict(self, myobj, resource):
+        
+        listChildren = myobj.get_children()
+        for child in listChildren:
+            self.nodeid_uri_dict[child.nodeid] = self.xae.find_uri(resource)
+    
+    def populate_dict_name(self, child):
+        self.nodeid_attr_dict[child.nodeid] = (child.get_browse_name().to_string())[2:]
 
-
+        
+#    def get_resource_uri(self, node):
+#        if node.get_child("2:parentID") is not None:
             
-        
-        
-        
-        
-        
-                
-    def initServer(self):
-        server = opcua.Server()
+    def init_server(self):
+        custom_iserver = CustomInternalServer(self)
+        server = opcua.Server(iserver=custom_iserver)
+        #server.iserver.isession.read = MethodType(manager.read_builder(), server.iserver.isession)
         server.set_endpoint("opc.tcp://0.0.0.0:4840/freeopcua/server/")
         server.import_xml(os.path.dirname(__file__) + '/onem2m-opcua.xml')
-        server.get_namespace_index("http://dieei.unict.it/oneM2M-OPCUA/")
         
         return server
                 
-    def nodeBuilder(self, resourceDiscovered,server):
+    def node_builder(self, resourceDiscovered,server):
         for resource in resourceDiscovered:
             if isinstance(resource, CSEBase):
-                cseNode = self.cseNodeBuilder(server, resource)
-            if isinstance(resource, AE):
-                self.aeNodes.append((self.aeNodeBuilder(cseNode, resource)))
-            if isinstance(resource, Container):
-                self.containerNodes.append(self.containerNodeBuilder(self.aeNodes,resource))
-            if isinstance(resource, ContentInstance):
-                self.contentInstanceNodeBuilder(self.containerNodes,resource)
-         
+                cseNode = self.cse_node_builder(server, resource)
+            elif isinstance(resource, AE):
+                self.aeNodes.append((self.ae_node_builder(cseNode, resource)))
+            elif isinstance(resource, Container):
+                self.containerNodes.append(self.container_node_builder(self.aeNodes,self.containerNodes, resource))
+            elif isinstance(resource, ContentInstance):
+                self.content_instance_node_builder(self.containerNodes,resource)
                 
-        
+    
+    def translate_request(self,node_to_read):
+            if self.nodeid_uri_dict.get(node_to_read, None) is not None:
+                onem2m_request = OneM2MRequest("retrieve", to=self.nodeid_uri_dict.get(node_to_read))
+                promise = self.xae.client.send_onem2m_request(onem2m_request)
+                onem2m_response = promise.get()
+                response = onem2m_response.content
+                attr_to_read = self.opc_openmtc_attrname_dict[self.nodeid_attr_dict[node_to_read]]
+                server.set_attribute_value(node_to_read, getattr(response,attr_to_read))
+                
+                datavalue =self.decode_response(attr_to_read, getattr(response,attr_to_read))
+                print(" Read_Value: "+ str(datavalue))
+                
+                server.get_node(node_to_read).set_value(datavalue)
+                
+    def parse_json(self):
+        f = open('opc_openmtc_name_map.json')
+        data = json.load(f)
+        f.close()
+        return data
+    
+    def decode_response(self, attr_to_read, datavalue):
+        if attr_to_read == "content":
+            return datavalue.decode("utf-8")
+        if attr_to_read == "cseType":
+            return datavalue-1
+        return datavalue
+            
 
-ipe = IpeAe()
-ipe.connect_to_local()
-manager = InterworkingManager(ipe)
-server = manager.initServer()
-manager.mapResourceToNode(server)
+
+#write (can update attributes only)
+#https://fiware-openmtc.readthedocs.io/en/latest/sdk-client/index.html 
+
+
+ipe = IpeAe("ipe_ae", ['http://localhost:21346'])
+ipe_ae_thread = Thread(target= ipe.connect_to_local)
+
+ipe_ae_thread.start()
+
+#need to initialize ipe_ae, can be used thread-lock
+time.sleep(7)
+ipe.retrieveRequest()
+print("RESOURCE DISCOVERED FROM IPE-AE:")
+print(ipe.resourceDiscovered)
+in_manager = InterworkingManager(ipe)
+server = in_manager.init_server()
+in_manager.map_resource_to_node(server)
 server.start()
-
-
-
-
 
 
 
--- IpeAe.py
+++ IpeAe.py
@@ -2,43 +2,114 @@ from openmtc_app.flask_runner import FlaskRunner
 from openmtc_app.onem2m import XAE
 from openmtc_onem2m import OneM2MRequest
 from openmtc_onem2m.client.http import OneM2MHTTPClient
-from openmtc_onem2m.model import AE, ResourceTypeE, CSEBase, Container, ContentInstance
+from openmtc_onem2m.model import AE, ResourceTypeE, CSEBase, Container, ContentInstance,\
+    Subscription,ResourceTypeE, EventNotificationCriteria, NotificationEventTypeE
+from time import sleep
+from random import random
+import threading
+#subscription net 1 regard update to all attr of subscribed resource
 
-
-
-
-client = OneM2MHTTPClient("http://localhost:8000", False)
 
 
 class IpeAe(XAE):
+    
+    def __init__(self, name_ae, poas):
+        XAE.__init__(self,name=name_ae,poas=poas)
+        
+        #self.lock = threading.Lock()
+        #self.lock.acquire()
+        
+
+        
+    remove_registration = True
+
+    # sensors to create
+    sensors = [
+        'tmp'
+
+    ]
+
+    # available actuators
+    actuators = [
+        'a1',    # AirConditioning
+             # Window
+    ]
+
+    # settings for random sensor data generation
+    threshold = 0.2
+    temp_range = 25
+    temp_offset = 10
+    humi_range = 50
+    humi_offset = 30
 
     resourceDiscovered = []
-
-    #ipe activity, actually make a retrieve request of all reosurce in cse-gateway
+    uri_resource_dict = {}
+    client = OneM2MHTTPClient("http://localhost:8000",False)
+    
     def _on_register(self):
-        self.retrieveRequest()
-        print(self.resourceDiscovered)
-        exit()
+        
+        # init variables
+        self._recognized_sensors = {}
+        self._recognized_measurement_containers = {}
+        self._command_containers = {}
 
+        # init base structure
+        label = 'devices'
+        container = Container(resourceName=label)
+        self._devices_container = self.create_container(None,
+                                                        container,
+                                                        labels=[label],
+                                                        max_nr_of_instances=0)
 
+        # create container for each actuator
+        for actuator in self.actuators:
+            actuator_container = Container(resourceName=actuator)
+            self.create_container(
+                self._devices_container.path,  # the target resource/path parenting the Container
+                actuator_container,            # the Container resource or a valid container ID
+                max_nr_of_instances=0,         # the container's max_nr_of_instances (here: 0=unlimited)
+                labels=['actuator']            # (optional) the container's labels
+            )
+            # create container for the commands of the actuators
+            commands_container = Container(resourceName='commands')
+            commands_container = self.create_container(
+                actuator_container.path,
+                commands_container,
+                max_nr_of_instances=3,
+                labels=['commands']
+            )
+            # add commands_container of current actuator to self._command_containers
+            self._command_containers[actuator] = commands_container
+            # subscribe to command container of each actuator to the handler command
+            self.add_container_subscription(
+                commands_container.path,    # the Container or it's path to be subscribed
+                self.handle_command         # reference of the notification handling function
+            )
 
-
-
-
-    #resource discovery, and append resource content in list
+        
+        # trigger periodically new data generation
+        self.run_forever(3, self.get_random_data)
+        print(self.uri_resource_dict)
+        # log message
+        self.logger.debug('registered')
+                #exit()
+        
     def retrieveRequest(self):
         content_request = self.discover()
         for resource in content_request:
             onem2m_request = OneM2MRequest("retrieve", to=resource)
-            promise = client.send_onem2m_request(onem2m_request)
+            promise = self.client.send_onem2m_request(onem2m_request)
             onem2m_response = promise.get()
             response = onem2m_response.content
+            
             self.resourceDiscovered.append(self.resourceRetrievedBuilder(response))
-
-    #switch-case resource builder,
+            self.uri_resource_dict[resource] = self.resourceRetrievedBuilder(response)
+        # to remove None values in list 
+        self.resourceDiscovered = [i for i in self.resourceDiscovered if i]
+    
     def resourceRetrievedBuilder(self, response):
         #AE-Builder
-        if response.resourceType == ResourceTypeE.AE:
+        if response.resourceType == ResourceTypeE.AE and response.AE_ID != "ipe-ae":
             return self.aeBuilder(response)
         #CSEBase-Builder
         elif response.resourceType == ResourceTypeE.CSEBase:
@@ -50,7 +121,6 @@ class IpeAe(XAE):
         elif response.resourceType == ResourceTypeE.contentInstance:
             return self.contentInstanceBuilder(response)
 
-
     def aeBuilder(self, response):
         ae = AE(resourceName=response.resourceName,
                 requestReachability=True,
@@ -58,8 +128,12 @@ class IpeAe(XAE):
                 resourceID=response.resourceID,
                 parentID=response.parentID,
                 lastModifiedTime=response.lastModifiedTime,
-                creationTime=response.creationTime, App_ID=response.App_ID,
-                AE_ID=response.AE_ID)
+                creationTime=response.creationTime, 
+                App_ID=response.App_ID,
+                AE_ID=response.AE_ID
+                )
+        print(ae)
+        self.print_resource(response)
 
         return ae
 
@@ -70,8 +144,8 @@ class IpeAe(XAE):
                           parentID=response.parentID,
                           lastModifiedTime=response.lastModifiedTime,
                           creationTime=response.creationTime,
-                          CSE_ID=response.id,
-                          cseType=response.cseType)
+                          CSE_ID=response.CSE_ID,
+                         cseType=response.cseType)
 
         return cseBase
 
@@ -83,6 +157,8 @@ class IpeAe(XAE):
                               lastModifiedTime=response.lastModifiedTime,
                               creationTime=response.creationTime,
                               currentNrOfInstances=response.currentNrOfInstances)
+        print(container)
+        self.print_resource(response)
 
         return container
 
@@ -95,14 +171,112 @@ class IpeAe(XAE):
                                           creationTime=response.creationTime,
                                           content=response.content,
                                           contentSize=response.contentSize)
-
+        self.print_resource(response)
         return contentInstance
 
     #def groupBuilder(self): not implemented in openmtc
-
-
+    
+    def print_resource(self,response):
+        print("ResourceName: "+response.resourceName)
+        print("ResourceID: "+response.resourceID)
+        print("ResourceType: "+str(response.resourceType))
+        print("ParentID:  "+response.parentID)
+        print("")
+        print("")
+        print("")
+    
+    
+    def find_uri(self,resource):
+        #la via del pitone
+        return next((k for k, v in self.uri_resource_dict.items() if v is not None and  v.resourceID == resource.resourceID), None)
+    #connect to local()
+    
     def connect_to_local(self):
+        print("IpeAe Starting....")
         runner = FlaskRunner(self)
         runner.run("http://localhost:8000")
+
+    def handle_command(self, container, value):
+            print('handle_command...')
+            print('container: %s' % container)
+            print('value: %s' % value)
+            print('')
+
+    def get_random_data(self):
+
+        # at random time intervals
+        if random() > self.threshold:
+
+            # select a random sensor
+            sensor = self.sensors[int(random() * len(self.sensors))]
+
+            # set parameters depending on sensor type
+            if sensor.startswith('Temp'):
+                value_range = self.temp_range
+                value_offset = self.temp_offset
+            else:
+                value_range = self.humi_range
+                value_offset = self.humi_offset
+
+            # generate random sensor data
+            value = int(random() * value_range + value_offset)
+            self.handle_sensor_data(sensor, value)
+
+    def handle_sensor_data(self, sensor, value):
+
+        # initialize sensor structure if never done before
+        if sensor not in self._recognized_sensors:
+            self.create_sensor_structure(sensor)
+        self.push_sensor_data(sensor, value)
+
+    def create_sensor_structure(self, sensor):
+        print('initializing sensor: %s' % sensor)
+
+        # create sensor container
+        device_container = Container(resourceName=sensor)
+        device_container = self.create_container(self._devices_container.path,
+                                                 device_container,
+                                                 labels=['sensor'],
+                                                 max_nr_of_instances=0)
+
+        # add sensor to _recognized_sensors
+        self._recognized_sensors[sensor] = device_container
+
+        # create measurements container
+        labels = ['measurements']
+        if sensor.startswith('Temp'):
+            labels.append('temperature')
+        else:
+            labels.append('humidity')
+        measurements_container = Container(resourceName='measurements')
+        measurements_container = self.create_container(device_container.path,
+                                                       measurements_container,
+                                                       labels=labels,
+                                                       max_nr_of_instances=3)
+
+        # add measurements_container from sensor to _recognized_measurement_containers
+        self._recognized_measurement_containers[sensor] = measurements_container
+
+    def push_sensor_data(self, sensor, value):
+
+        # build data set with value and metadata
+        if sensor.startswith('Temp'):
+            data = {
+                'value': value,
+                'type': 'temperature',
+                'unit': 'degreeC'
+            }
+        else:
+            data = {
+                'value': value,
+                'type': 'humidity',
+                'unit': 'percentage'
+            }
+
+        # print the new data set
+        print ('%s: %s' % (sensor, data))
+
+        # finally, push the data set to measurements_container of the sensor
+        self.push_content(self._recognized_measurement_containers[sensor], data)
 
 
